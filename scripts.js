6b6e174cb475d1102ff15a58419c2555997fbfb5
Thien Pham <73629620+TCPham@users.noreply.github.com>
2023-03-10 13:03:22 -0600

    Added new pics

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 const cards = document.querySelectorAll('.matching-card');
 
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
